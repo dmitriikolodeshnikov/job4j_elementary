be9f37c1cfbb5abfb39740239ddaa329c39f078d
dmitriikolodeshnikov <kolodeshnikov-90@mail.ru>
2023-08-11 19:09:38 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,6 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен " + prices.length);
-
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Dmirtii Kolodeshnikov";
+        names[2] = "Sergei Orlov";
+        names[3] = "Roman Bubnev";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
